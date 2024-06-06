5838b05706ff83767c90a858f740a6329c4ce39a
Marek Kopli <Marek.Kopli@tptlive.ee>
2024-06-06 23:18:59 +0300

    first commit

---
+++ home.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        if (navLinks.style.display === 'flex') {
+            navLinks.style.display = 'none';
+        } else {
+            navLinks.style.display = 'flex';
+        }
+    });
+});
